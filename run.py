7dfeed97fb2d1997bf7f582ef65b83ad2183ad34
Bonoldie <erribono01@gmail.com>
2025-07-16 16:56:12 +0200

    run script stub

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import open3d as o3d
 import numpy as np
 from scipy.spatial.distance import cdist
+import json
 
 pairs_path = [
     # (reference_path, target_path)
@@ -13,7 +14,7 @@ pairs_path = [
 ]
 
 
-def loadPairs():
+def loadPairs(pairs_path):
     pairs = []
 
     for pair_path in pairs_path:
@@ -23,11 +24,23 @@ def loadPairs():
     
     return pairs
 
+camera_matrix = np.array([
+    [5794.23954825, 5.00502053223e-12, 2792.29091924],
+    [0.0,           5794.23954825,     1817.35747811],
+    [0.0,           0.0,               1.0]
+])
+dist_coeffs = np.array([
+    -0.049194332605,   # k1
+     0.0277098399156,  # k2
+     0.0,              # p1
+     0.0,              # p2
+     0.12963081329     # k3
+])
+
 def deep_match(reference, target, visibility_reference,  model_pointcloud, match_max_distance=3):
 
     reference_vis_array = convert_coords(np.asarray([*map( lambda vis_entry: [vis_entry['w'], vis_entry['h']], visibility_reference)]), width=reference.shape[1], height=reference.shape[0] )
     
-
     deep_match_results = {
         'reference_image': reference,
         'target_image': target,
@@ -38,6 +51,8 @@ def deep_match(reference, target, visibility_reference,  model_pointcloud, match
     deep_match_results['by_model']['RDD'] = {'matches': evaluateRDD(reference, target)}
 
 
+    pc_array = np.asarray(model_pointcloud.points)
+
     for model in  deep_match_results['by_model'].keys():
         reference_matches = deep_match_results['by_model'][model]['matches']['reference_matches']
         target_matches = deep_match_results['by_model'][model]['matches']['target_matches']
@@ -47,13 +62,28 @@ def deep_match(reference, target, visibility_reference,  model_pointcloud, match
 
         dist_indices = np.argwhere(dist_mask == True)
 
-        deep_match_results['by_model'][model]['pose_data'] = np.array([])
+        deep_match_results['by_model'][model]['pose_data'] = {'target_points': [], 'model_points': []}
 
-        for indices in dist_indices:
-            pass
+        for ref_index, vis_index in dist_indices:
+            target_2d_point = target_matches[ref_index]
+            model_3d_point_index = visibility_reference[vis_index]['index']
+            model_3d_point = pc_array[model_3d_point_index]
+            
+            deep_match_results['by_model'][model]['pose_data']['target_points'].append(target_2d_point)
+            deep_match_results['by_model'][model]['pose_data']['model_points'].append(model_3d_point)
 
+        deep_match_results['by_model'][model]['pose_data']['target_points'] = np.asanyarray(deep_match_results['by_model'][model]['pose_data']['target_points'])
+        deep_match_results['by_model'][model]['pose_data']['model_points'] = np.asanyarray(deep_match_results['by_model'][model]['pose_data']['model_points'])
+        
+        print(deep_match_results['by_model'][model]['pose_data']['target_points'].shape)
+        print(deep_match_results['by_model'][model]['pose_data']['model_points'].shape)
 
+        success, rvec, tvec = cv2.solvePnP(deep_match_results['by_model'][model]['pose_data']['model_points'], deep_match_results['by_model'][model]['pose_data']['target_points'], cameraMatrix=camera_matrix, distCoeffs=dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
 
+        deep_match_results['by_model'][model]['pose_data']['success'] = success
+        rmat, _ = cv2.Rodrigues(rvec)
+        deep_match_results['by_model'][model]['pose_data']['R'] = rmat 
+        deep_match_results['by_model'][model]['pose_data']['t'] = tvec
 
 
         # matches_mask = np.any(dist <= match_max_distance, axis=1)
@@ -78,9 +108,13 @@ if __name__ == '__main__':
     
     for (pair_index, pair) in enumerate(pairs):
         
-        visibility_reference = visibility_map.get(Path(pairs_path[pair_index]).stem)
+        visibility_reference = visibility_map.get(Path(pairs_path[pair_index][0]).name)
 
         pairs_match_results.append(deep_match(pair[0], pair[1], visibility_reference, pc))
+
+    
+    print(pairs_match_results[0]['by_model']['RDD']['pose_data']['R'])
+    print(pairs_match_results[0]['by_model']['RDD']['pose_data']['t'])
 
 
 
